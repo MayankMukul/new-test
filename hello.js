3b336c3da341776040415c42f102ea58b6462ebc
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-12 17:27:17 +0530

    practise question

--- hello.js
+++ hello.js
@@ -1,10 +1,10 @@
-console.log("Hello");
-let arr = [34,43,45,65];
-console.log(Array.from(arr));
+// console.log("Hello");
+// let arr = [34,43,45,65];
+// console.log(Array.from(arr));
 
 //practice problem
-let a = [2,3,4,6];
-console.log(a);
+//let a = [2,3,4,6];
+//console.log(a);
 // let element = prompt('enter a number');
 // element = Number.parseInt(element)
 // arr.push(element);
@@ -18,5 +18,29 @@ console.log(a);
 //     element = Number.parseInt(element)
 //     arr.push(element);
 // } 
-
 // console.log(arr);
+
+//Practise Problem 
+// let a = [2,3,4,6,45,98,34,76,7];
+// console.log(a);
+// let n = a.filter((a)=>{
+//     return a>10 ;
+// })
+// console.log(n);
+
+//Practise Problem 
+// let a = [2,3,4,6,45,98,34,76,7];
+// console.log(a);
+// let n = a.map((a)=>{
+//     return a*a ;
+// })
+// console.log(n);
+
+
+//Practise Problem 
+// let a = [2,3,4,5,6,7];
+// console.log(a);
+// let n = a.reduce((a,b)=>{
+//     return a*b ;
+// })
+// console.log(n);
